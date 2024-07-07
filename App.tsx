1997d2593b4204d4f8b86cafe50ac944b47258e3
Shahir BIn Zulfiker <2105149@ugrad.cse.buet.ac.bd>
2024-07-07 20:24:49 +0600

    navigation shit is fixed

--- App.tsx
+++ App.tsx
@@ -3,14 +3,18 @@ import Login from './src/screen/Login';
 import Register from './src/screen/Register';
 
 import {View, Text, SafeAreaView} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <SafeAreaView>
-      <View>
-      <Login/>
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
